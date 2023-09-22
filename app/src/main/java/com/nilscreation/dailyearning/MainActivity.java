a3a182b2a983c6699a20f26f5000e5911c1f61c6
admin <nileshsonawanen1@gmail.com>
2023-09-22 10:43:37 +0530

    Share app

--- app/src/main/java/com/nilscreation/dailyearning/MainActivity.java
+++ app/src/main/java/com/nilscreation/dailyearning/MainActivity.java
@@ -1,20 +1,53 @@
 package com.nilscreation.dailyearning;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
         loadFragment(new MainFragment());
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (id == R.id.home) {
+                    loadFragment(new MainFragment());
+                } else if (id == R.id.settings) {
+                    loadFragment(new SettingsFragment());
+                } else {
+                    String url = "For Daily Earning download the app now " + "\nhttps://play.google.com/store/apps/details?id=" + getApplicationContext().getPackageName();
+                    Intent share = new Intent(Intent.ACTION_SEND);
+                    share.setType("text/plain");
+                    share.putExtra(Intent.EXTRA_SUBJECT, "DOWNLOAD NOW");
+                    share.putExtra(Intent.EXTRA_TEXT, url);
+                    startActivity(Intent.createChooser(share, "Share via"));
+                }
+
+                return true;
+            }
+        });
 
     }
 
@@ -25,5 +58,31 @@ public class MainActivity extends AppCompatActivity {
         ft.commit();
     }
 
+    @Override
+    public void onBackPressed() {
+        exitDialog();
+    }
 
+    private void exitDialog() {
+        AlertDialog.Builder exit = new AlertDialog.Builder(MainActivity.this);
+        exit.setTitle("Exit");
+        exit.setMessage("Do you really want to Exit?");
+        exit.setIcon(R.drawable.logo);
+        exit.setCancelable(false);
+
+        exit.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finishAffinity();
+            }
+        });
+        exit.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        exit.show();
+
+    }
 }
